f84acbf0e8f6a35c79d3e3a18d9dd03012d039b5
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-15 17:27:45 -0500

    se conecta con BD y se hace la consulta

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -5,3 +5,26 @@ use DBI;
 use CGI;
 my $q = CGI->new;
 print $q->header("text/html","UTF-8");
+
+my $user = "alumno";
+my $password = "pweb1";
+my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
+
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+my $stm = "SELECT Title FROM Articles";
+my @titles = consulta($stm);
+$dbh->disconnect;
+
+
+sub consulta{
+  my $condicion = $_[0];
+  my $sth = $dbh->prepare($condicion);
+  $sth->execute();
+  my @titles;
+  while(my @row = $sth->fetchrow_array){
+    push @titles,$row[0];
+  }
+  $sth->finish;
+  return @titles;
+}
