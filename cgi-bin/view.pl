d239e5dba7aa70d09d3aebd71806a90859f89da8
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-15 17:38:24 -0500

    se creo tags y expresiones regualres

--- cgi-bin/view.pl
+++ cgi-bin/view.pl
@@ -17,3 +17,64 @@ my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 my $stm = "SELECT Text FROM Articles WHERE Title=?";
 my $text = consulta($stm,$title);
 $dbh->disconnect;
+
+
+my @types = (
+  {
+    regex => '^\s{0,3}# (.*$)',
+    tag => 'h1'
+  },
+  {
+    regex => '^\s{0,3}## (.*$)',
+    tag => 'h2'
+  },
+  {
+    regex => '^\s{0,3}### (.*$)',
+    tag => 'h3'
+  },
+  {
+    regex => '^\s{0,3}##### (.*$)',
+    tag => 'h4'
+  },
+  {
+    regex => '^\s{0,3}##### (.*$)',
+    tag => 'h5'
+  },
+  {
+    regex => '^\s{0,3}###### (.*$)',
+    tag => 'h6'
+  },
+  {
+    regex => '\*\*\*(.+)\*\*\*',
+    tag => 'b'
+  },
+  {
+    regex => '\*\*(.+)\*\*',
+    tag => 'strong'
+  },
+  {
+    regex => '\*(.+)\*',
+    tag => 'em'
+  },
+  {
+    regex => '_(.+)_',
+    tag => 'em'
+  },
+
+  {
+    regex => '~~(.+)~~',
+    tag => 'del'
+  },
+  {
+    regex=> '\[(.+)\]\((.+)\)',
+    tag=> 'a'
+  },
+  {
+    regex => '```([\s\S.]+)```',
+    tag => 'code'
+  },
+  {
+    regex => '(((?:^[^<])|(?:<(?:b|strong|del|em)>)).+((?:[^>]$)|(?:<\/(?:b|strong|del|em)>)))',
+    tag => 'p'
+  }
+ );
