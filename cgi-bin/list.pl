2799c5caa6fe3d86a6fc2c92b91dad2ef5b2f33e
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-15 17:28:39 -0500

    se crea render lista y printhtml

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -16,6 +16,9 @@ my $stm = "SELECT Title FROM Articles";
 my @titles = consulta($stm);
 $dbh->disconnect;
 
+my $list = renderLista(@titles);
+printHTML("../estilos.css",$list);
+
 
 sub consulta{
   my $condicion = $_[0];
@@ -27,4 +30,48 @@ sub consulta{
   }
   $sth->finish;
   return @titles;
+}
+
+sub renderLista{ 
+  my @titles = @_;
+  my $lista="   <ul>\n";
+  foreach my $titulo (@titles){
+    my $div= <<"LISTA";
+      <li>
+        <a href="view.pl?title=$titulo">$titulo</a>
+        <section class ="boton-edicion">
+          <a href="delete.pl?title=$titulo">X</a>
+          <a href="edit.pl?title=$titulo">E</a>
+        </section> 
+      </li>
+LISTA
+    $lista.=$div;
+  }
+  $lista.="    </ul>";
+  return $lista;
+}
+sub printHTML{
+ my $css = $_[0];
+ my $body = $_[1];
+ print <<BLOCK;
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title></title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="$css">
+  </head>
+  <body>
+  <h1>Nuestras Paginas de Wiki</h1>
+$body
+  <section class="navegacion">
+    <nav class="navegacion">
+      <a href="../new.html">Nueva Pagina</a>
+      <a href="../qq.html">Volver al inicio</a>
+    </nav>
+  </section>
+  </body>
+</html>
+BLOCK
+
 }
