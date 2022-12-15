addd9509cef91d10709573f18e49203b1ce80eda
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-15 17:25:07 -0500

    se conecta con la base de datos

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -4,14 +4,25 @@ use warnings;
 use CGI;
 use DBI;
 
+my $user = "alumno";
+my $password = "pweb1";
+my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
+my $stm = "SELECT Title FROM Articles WHERE Title=?";
 
-
-
-
-
-
-
+sub select_title{
+  my $condicion = $_[0];
+  my $variable1 =$_[1];
+  my $sth = $dbh->prepare($condicion);
+  $sth->execute($variable1);
+  my $title;
+  while(my @row = $sth->fetchrow_array){
+    $title = $row[0];
+  }
+  $sth->finish;
+  return $title;
+}
 sub consulta{
   my $condicion = $_[0];
   my $variable1 =$_[1];
